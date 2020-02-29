e1744c7ac475398fa0624e6fb6e82825529e25df
even <evenliew@163.com>
2020-02-29 19:46:14 +0800

    init projet

---
+++ src/main/java/com/woniu/springboot/team2project/Team2projectApplication.java
@@ -0,0 +1,13 @@
+package com.woniu.springboot.team2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woniu/springboot/myspringboot00/Myspringboot00ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.woniu.springboot.myspringboot00;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Myspringboot00ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
